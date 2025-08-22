5c9b01fa9ef7c97a70b368db870e2261fa2422a3
Teodora <tedy.aa@abv.bg>
2025-08-22 14:26:55 +0300

    added footer and navbar

---
+++ vendor/javascript/all.js
@@ -0,0 +1,4 @@
+// all@0.0.0 downloaded from https://ga.jspm.io/npm:all@0.0.0/index.js
+
+var n={};n=function(n){var r=0,t=null;return function(){r++;return function(){--r||n()}}};var r=n;export default r;
+
