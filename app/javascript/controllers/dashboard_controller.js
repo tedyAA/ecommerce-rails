aef161bc8bec5c3cae5f74f0e58329b87e212396
Teodora <tedy.aa@abv.bg>
2025-08-24 12:30:00 +0300

    fixed chart not showing

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -1,53 +1,49 @@
+import "chart.js"
 import { Controller } from "@hotwired/stimulus"
-import {Chart, registerables } from 'chart.js'
 
-Chart.register(...registerables)
-
-// Connects to data-controller="dashboard"
 export default class extends Controller {
   static values = { revenue: Array }
 
-  initialize() {
-    const data = this.revenueValue.map((item) => item[1]/100.0)
+  connect() {
+    const Chart = window.Chart
     const labels = this.revenueValue.map((item) => item[0])
+    const data = this.revenueValue.map((item) => item[1]/100.0)
 
-    const ctx = document.getElementById('revenueChart')
-
+    const ctx = this.element.querySelector("#revenueChart").getContext("2d")
     new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Revenue $',
-          data: data,
-          borderWidth: 3,
-          fill: true
-        }]
-      },
-      options: {
-        plugins: {
-          legend: {
-            display: false
-          }
-        },
-        scales: {
-          x: {
-            grid: {
-              display: false
-            }
-          },
-          y: {
-            border: {
-              dash: [5, 5]
-            },
-            grid: {
-              color: "#d4f3ef"
-            },
-            beginAtZero: true
-          }
-        }
-      }
-    })
-
+         type: 'line',
+         data: {
+           labels: labels,
+           datasets: [{
+             label: 'Revenue $',
+             data: data,
+             borderWidth: 3,
+             fill: true
+           }]
+         },
+         options: {
+           plugins: {
+             legend: {
+               display: false
+             }
+           },
+           scales: {
+             x: {
+               grid: {
+                 display: false
+               }
+             },
+             y: {
+               border: {
+                 dash: [5, 5]
+               },
+               grid: {
+                 color: "#d4f3ef"
+               },
+               beginAtZero: true
+             }
+           }
+         }
+       })
   }
 }
