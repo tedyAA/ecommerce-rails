8070f0a25516269e4cd3e101fa32459b161846b8
Teodora <tedy.aa@abv.bg>
2025-08-24 12:30:09 +0300

    added charts js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "chart.js" // this just loads it globally
+const Chart = window.Chart
+
+
+
