9397fd361ce124f697f3272b64f0980ed44f1e0f
Teodora <tedy.aa@abv.bg>
2025-08-25 17:43:58 +0300

    added parameter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_14_140604) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_25_135029) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -104,6 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_14_140604) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "bestseller", default: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
